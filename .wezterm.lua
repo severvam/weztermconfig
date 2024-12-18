70577326532aa72cc6c7b16b977ed40c3bf1b510
severvam <alexander.severgin@gmail.com>
2024-12-18 09:38:22 +0200

    wezterm config and readme

---
+++ .wezterm.lua
@@ -0,0 +1,37 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+
+config.color_scheme = 'Ubuntu'
+
+config.font = wezterm.font('JetBrainsMono Nerd Font')
+config.font_size = 13
+config.line_height = 1.1
+
+config.window_background_opacity = 0.8
+config.macos_window_background_blur = 60
+
+config.initial_rows = 30
+config.initial_cols = 140
+
+config.window_frame = {
+ font = wezterm.font { family = 'Noto Sans', weight = 'Regular' },
+}
+
+
+local act = wezterm.action
+
+config.keys = {
+  { key = 'LeftArrow', mods = 'OPT', action = act.SendKey { key = 'b', mods = 'ALT', }, },
+  { key = 'RightArrow', mods = 'OPT', action = act.SendKey { key = 'f', mods = 'ALT' }, },
+  { key = 'w', mods = 'CMD', action = act.CloseCurrentTab{ confirm = false }, },
+  { key = 'f', mods = 'CMD', action = act.SplitVertical { domain = 'CurrentPaneDomain' }, },
+  { key = 'd', mods = 'CMD', action = act.SplitHorizontal { domain = 'CurrentPaneDomain' }, },
+  { key = 'h', mods = 'CMD', action = act.ActivatePaneDirection 'Left', },
+  { key = 'l', mods = 'CMD', action = act.ActivatePaneDirection 'Right', },
+  { key = 'j', mods = 'CMD', action = act.ActivatePaneDirection 'Down', },
+  { key = 'k', mods = 'CMD', action = act.ActivatePaneDirection 'Up', },
+  { key = 'x', mods = 'CMD', action = act.CloseCurrentPane{ confirm = false } },
+}
+
+
+return config
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # weztermconfig
+
+Install 'Jetbrains Mono Nerd Font', to use this config
+
+```shell
+brew install --cask font-jetbrains-mono-nerd-font
+```
